dc3159cc0f428e4139f5de79594490d8063ac41c
andika <zoneandika@gmail.com>
2023-08-01 14:41:43 +0700

    update lobi lagi

--- src/page/LobiMurid.jsx
+++ src/page/LobiMurid.jsx
@@ -14,19 +14,20 @@ import burungKanan from "../assets/burungKanan.svg";
 const LobiMurid = () => {
   const [nis, setNIS] = useState(undefined);
   const [nama, setNama] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const [api, contextHolder] = notification.useNotification();
 
   const handleClick = async () => {
     // query to api by nis
-    console.log(nis);
+    setLoading(true)
     axios
       .post("https://joyous-fashion-frog.cyclic.app/user/profile/", {
         nis: parseInt(nis),
       })
       .then((res) => {
-        console.log(res.data.nama);
         setNama(res.data.nama);
+        setLoading(false)
       })
       .catch((err) => {
         console.log("nis tidak ditemukan");
@@ -34,10 +35,21 @@ const LobiMurid = () => {
       });
   };
 
-  console.log(nama);
+  console.log(loading)
 
   return (
-    <div>
+    <div className="relative">
+      {/* loading section */}
+      { loading === true ? (
+        <div className="w-full h-full absolute top-0 bg-slate-700 z-50 opacity-50 flex">
+          <h1 className="text-center text-white my-[500px] mx-auto opacity-100">loading...</h1>
+        </div>
+      ) : (
+        <></>
+      )
+      }
+
+
       <NavBar display="logout" route="" />
       {contextHolder}
       <div className="py-[180px] pb-[200px] w-full bg-[#ECAA55]">
@@ -45,9 +57,9 @@ const LobiMurid = () => {
           {nama !== null ? (
             <>
               <h1
-                className={`font-poppins text-[3rem] md:text-[3rem] text-center md:leading-[40px] leading-[40px] duration-[2s] ease-out w-[90%] mx-auto text-white`}
+                className={`font-poppins text-[3rem] md:text-[3rem] text-center md:leading-[55px] leading-[40px] duration-[2s] ease-out w-full mx-auto text-white`}
               >
-                Hallo {nama},
+                Hi {nama},
               </h1>
               <p className="text-center text-white text-[.9rem]">
                 silahkan pilih kandidat sesuai pilihan yang anda suka
@@ -103,6 +115,25 @@ const LobiMurid = () => {
             className="absolute scale-75 left-40 top-40"
             alt=""
           />
+        </div>
+      </div>
+
+      {/* pilih kandidat section */}
+      <div className="w-full bg-[#3C302A] pb-[50px]">
+        <div className="pt-[130px]">
+          <h1 className="text-[3rem] text-white text-center font-poppins">Submit Pilihanmu</h1>
+        </div>
+
+        <div className="flex gap-[50px] justify-center mx-auto w-[80%] my-[100px]">
+          <button className="py-[8px] px-[20px] bg-white rounded-[5px] text-[1.5rem] font-poppins">kandidat 1</button>
+          <button className="py-[8px] px-[20px] bg-white rounded-[5px] text-[1.5rem] font-poppins">kandidat 1</button>
+          <button className="py-[8px] px-[20px] bg-white rounded-[5px] text-[1.5rem] font-poppins">kandidat 1</button>
+        </div>
+
+        <div>
+          <button className="py-[8px] px-[20px] bg-white rounded-[5px] text-[1.5rem] font-poppins block ml-auto mr-[30px]">
+            submit
+          </button>
         </div>
       </div>
     </div>
