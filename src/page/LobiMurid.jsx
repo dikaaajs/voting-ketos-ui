a38f2f8e3dcc787aa8c1ba1f85c65796de7c1fcc
andika <zoneandika@gmail.com>
2023-08-03 01:04:27 +0700

    update button ketos

--- src/page/LobiMurid.jsx
+++ src/page/LobiMurid.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import NavBar from "../components/NavBar";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -10,45 +10,58 @@ import { notification } from "antd";
 import headerKandidat from "../assets/headerKandidat.svg";
 import burungKiri from "../assets/burungKiri.svg";
 import burungKanan from "../assets/burungKanan.svg";
+import centang from "../assets/check-circle-svgrepo-com (1).svg";
 
 const LobiMurid = () => {
   const [nis, setNIS] = useState(undefined);
   const [nama, setNama] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  const [btn1Clicked, setBtn1Clicked] = useState(false);
+  const [btn2Clicked, setBtn2Clicked] = useState(false);
+  const [btn3Clicked, setBtn3Clicked] = useState(false);
+
+  // antd setting
   const [api, contextHolder] = notification.useNotification();
+  const notif = (type, msg, des) => {
+    api[type]({
+      message: msg,
+      description: des,
+    });
+  };
 
   const handleClick = async () => {
     // query to api by nis
-    setLoading(true)
+    setLoading(true);
     axios
-      .post("https://joyous-fashion-frog.cyclic.app/user/profile/", {
+      .post("https://joyous-fashion-frog.cyclic.app/student/profile", {
         nis: parseInt(nis),
       })
       .then((res) => {
+        notif("success", "login berhasil", "nis yang dimasukan sudah valid");
         setNama(res.data.nama);
-        setLoading(false)
+        setLoading(false);
       })
       .catch((err) => {
-        console.log("nis tidak ditemukan");
-        console.log(err.message);
+        notif("warning", "ada yang salah", err.message);
+        setLoading(false);
       });
   };
 
-  console.log(loading)
+  console.log(loading);
 
   return (
     <div className="relative">
       {/* loading section */}
-      { loading === true ? (
+      {loading === true ? (
         <div className="w-full h-full absolute top-0 bg-slate-700 z-50 opacity-50 flex">
-          <h1 className="text-center text-white my-[500px] mx-auto opacity-100">loading...</h1>
+          <h1 className="text-center text-white my-[500px] mx-auto opacity-100">
+            loading...
+          </h1>
         </div>
       ) : (
         <></>
-      )
-      }
-
+      )}
 
       <NavBar display="logout" route="" />
       {contextHolder}
@@ -64,12 +77,12 @@ const LobiMurid = () => {
               <p className="text-center text-white text-[.9rem]">
                 silahkan pilih kandidat sesuai pilihan yang anda suka
               </p>
-              <Link
-                className="button-login font-poppins !text-[1.4rem] my-[50px] !px-[15px] block w-fit"
+              <a
+                className="text-[1rem] font-semibold font-lato my-[50px] px-[20px] py-[8px] block w-fit border-orange-950 border-[1px] rounded-full text-orange-950 mx-auto"
                 to={"/coblos/ketos"}
               >
                 coblos
-              </Link>
+              </a>
             </>
           ) : (
             <div className="relative">
@@ -121,17 +134,74 @@ const LobiMurid = () => {
       {/* pilih kandidat section */}
       <div className="w-full bg-[#3C302A] pb-[50px]">
         <div className="pt-[130px]">
-          <h1 className="text-[3rem] text-white text-center font-poppins">Submit Pilihanmu</h1>
+          <h1 className="text-[3rem] text-white text-center font-poppins">
+            Submit
+          </h1>
+          <p className="text-white text-center">pilih dan submit kandidat</p>
         </div>
 
         <div className="flex gap-[50px] justify-center mx-auto w-[80%] my-[100px]">
-          <button className="py-[8px] px-[20px] bg-white rounded-[5px] text-[1.5rem] font-poppins">kandidat 1</button>
-          <button className="py-[8px] px-[20px] bg-white rounded-[5px] text-[1.5rem] font-poppins">kandidat 1</button>
-          <button className="py-[8px] px-[20px] bg-white rounded-[5px] text-[1.5rem] font-poppins">kandidat 1</button>
+          <button
+            className={`py-[10px] px-[25px] bg-white rounded-[5px] text-[.8rem] font-courier relative duration-300 ease-out ${
+              btn1Clicked && "btn-ketos-active"
+            }`}
+            onClick={() => {
+              setBtn2Clicked(false);
+              setBtn3Clicked(false);
+              setBtn1Clicked(true);
+            }}
+          >
+            {btn1Clicked && (
+              <img
+                src={centang}
+                alt=""
+                className="scale-50 absolute bottom-[2px] z-20 right-[-25px]"
+              />
+            )}
+            kandidat 1
+          </button>
+          <button
+            className={`py-[10px] px-[25px] bg-white rounded-[5px] text-[.8rem] font-courier relative duration-300 ease-out ${
+              btn2Clicked && "btn-ketos-active"
+            }`}
+            onClick={() => {
+              setBtn1Clicked(false);
+              setBtn3Clicked(false);
+              setBtn2Clicked(true);
+            }}
+          >
+            {btn2Clicked && (
+              <img
+                src={centang}
+                alt=""
+                className="scale-50 absolute bottom-[2px] z-20 right-[-25px]"
+              />
+            )}
+            kandidat 2
+          </button>
+          <button
+            className={`py-[10px] px-[25px] bg-white rounded-[5px] text-[.8rem] font-courier relative duration-300 ease-out ${
+              btn3Clicked && "btn-ketos-active"
+            }`}
+            onClick={() => {
+              setBtn1Clicked(false);
+              setBtn2Clicked(false);
+              setBtn3Clicked(true);
+            }}
+          >
+            {btn3Clicked && (
+              <img
+                src={centang}
+                alt=""
+                className="scale-50 absolute bottom-[2px] z-20 right-[-25px]"
+              />
+            )}
+            kandidat 3
+          </button>
         </div>
 
         <div>
-          <button className="py-[8px] px-[20px] bg-white rounded-[5px] text-[1.5rem] font-poppins block ml-auto mr-[30px]">
+          <button className="py-[10px] px-[25px] bg-white rounded-[5px] text-[.8rem] font-courier block ml-auto mr-[30px]">
             submit
           </button>
         </div>
